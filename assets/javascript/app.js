dc09e018d9454575f174065be494615f86173574
Brian O'Connell <accelmtgcomcast.net>
2018-03-15 18:31:21 -0500

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    
+    <script type="text/javascript"></script>
+
+
+$("#submit").click(function() {
+    var val = $('input[name=q12_3]:checked').val();
+    alert(val);
+  });
+
+  
+
